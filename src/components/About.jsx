a011b3e6be4f9468f6e05d5eb2caac34f659a144
Krish Patel <124129235+kri-eng@users.noreply.github.com>
2024-12-27 13:50:28 -0600

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -9,7 +9,7 @@ const About = () => {
         <div className="flex flex-col items-center">
           <div className="w-full md:w-2/3 space-y-12">
             <p className="text-lg text-gray-700">
-              As a Computer Science graduate from the University of Illinois at Chicago and a current Master's student at the Georgia Institute of Technology, I possess a robust foundation in programming, problem-solving, and software development. My academic journey has been complemented by practical experiences and a passion for iOS mobile engineering.
+              As a Computer Science graduate from the University of Illinois at Chicago and a current Master's student at the Georgia Institute of Technology, I possess a robust foundation in programming, problem-solving, and software development. My academic journey has been complemented by practical experiences and a passion for Artifical Intelligence, Machine Learning, and IOS Development.
             </p>
             <p className="text-lg text-gray-700">
               Through a comprehensive iOS development bootcamp, I gained hands-on experience in Swift, SwiftUI, API integration, JSON parsing, asynchronous communication, and local data persistence. My GitHub repositories showcase my proficiency in developing multi-screen iOS applications, implementing features like search, filtering, and mapping, and integrating third-party libraries and services such as Firebase.
@@ -21,7 +21,7 @@ const About = () => {
               Beyond mobile and web development, I have explored Machine Learning and AI through projects like Cardiovascular Disease Detection and the 15 Puzzle Solver. These experiences have honed my ability to apply advanced algorithms, heuristics, and optimization techniques to solve complex problems, achieving notable results such as a 91% classification accuracy in cardiovascular disease prediction.
             </p>
             <p className="text-lg text-gray-700">
-              With a keen interest in continuous learning and staying up-to-date with emerging technologies, I am actively seeking opportunities to contribute my skills and passion for iOS mobile engineering to innovative projects. My goal is to leverage my diverse technical skill set, from Python and Java to AWS and React, to build solutions that drive efficiency and enhance user experiences.
+              With a keen interest in continuous learning and staying up-to-date with emerging technologies, I am actively seeking opportunities to contribute my skills and passion to innovative projects. My goal is to leverage my diverse technical skill set, from Python and Java to AWS and React, to build solutions that drive efficiency and enhance user experiences.
               <br />
               <br />
               Feel free to explore my work on GitHub and connect with me on LinkedIn. Let's collaborate and create something impactful together!
